df6ec8d8f1c8e5ab3add7337b951f3a2cea924d5
Jacob S. Steward <JacobTheEldest@gmail.com>
2023-10-22 14:15:31 +0000

    combine archives

--- decrypt.sh
+++ decrypt.sh
@@ -5,7 +5,3 @@ SCRIPT_DIR=$(dirname "$0")
 gpg --decrypt $SCRIPT_DIR/sensitive.tar.gpg > $SCRIPT_DIR/sensitive.tar
 tar -C $SCRIPT_DIR -xvf $SCRIPT_DIR/sensitive.tar
 rm $SCRIPT_DIR/sensitive.tar
-
-gpg --decrypt $SCRIPT_DIR/ansible.tar.gpg > $SCRIPT_DIR/ansible.tar
-tar -C $SCRIPT_DIR -xvf $SCRIPT_DIR/ansible.tar
-rm $SCRIPT_DIR/ansible.tar
--- encrypt.sh
+++ encrypt.sh
@@ -2,10 +2,6 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
-tar -C $SCRIPT_DIR -cvf $SCRIPT_DIR/sensitive.tar sensitive/
+tar -C $SCRIPT_DIR -cvf $SCRIPT_DIR/sensitive.tar sensitive/ ansible/
 gpg --yes --symmetric --no-symkey-cache --cipher-algo AES256 $SCRIPT_DIR/sensitive.tar
 rm $SCRIPT_DIR/sensitive.tar
-
-tar -C $SCRIPT_DIR -cvf $SCRIPT_DIR/ansible.tar ansible/
-gpg --yes --symmetric --no-symkey-cache --cipher-algo AES256 $SCRIPT_DIR/ansible.tar
-rm $SCRIPT_DIR/ansible.tar
