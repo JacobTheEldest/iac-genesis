f63bfc35c0c60efdb983d5215aa4cdac48a98c4f
Jacob S. Steward <JacobTheEldest@gmail.com>
2023-10-22 04:44:25 +0000

    Add basic setup

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Use the `initial_setup.sh` script to pull this github repository
+`wget -q -O - https://raw.githubusercontent.com/JacobTheEldest/iac-master/master/initial_setup.sh`
+
---
+++ initial_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+
+
+sudo apt update
+sudo apt install -y git gpg tar
+
+
+git clone --depth=1 https://github.com/JacobTheEldest/iac-master
+
+$SCRIPT_DIR/iac-master/decrypt.sh
