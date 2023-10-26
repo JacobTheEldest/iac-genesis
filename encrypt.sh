70c52b0351e9ef4e0e0c7869c896076d6f2055a4
Jacob S. Steward <jacobtheeldest@gmail.com>
2023-10-26 01:01:17 +0000

    Encrypt only secrets. Use version control for all else

--- encrypt.sh
+++ encrypt.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
-tar -C $SCRIPT_DIR -cvf $SCRIPT_DIR/sensitive.tar sensitive/ ansible/
+tar -C $SCRIPT_DIR -cvf $SCRIPT_DIR/sensitive.tar sensitive/ ansible/vault_password
 gpg --yes --symmetric --no-symkey-cache --cipher-algo AES256 $SCRIPT_DIR/sensitive.tar
 rm $SCRIPT_DIR/sensitive.tar
