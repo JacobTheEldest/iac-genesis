d483d81162a7901792aac57597c2aa39fd53fc78
Jacob S. Steward <jacobtheeldest@gmail.com>
2023-10-28 18:18:11 +0000

    Add fix_git.sh to encrypt script

--- encrypt.sh
+++ encrypt.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
-tar -C $SCRIPT_DIR -cvf $SCRIPT_DIR/sensitive.tar sensitive/ ansible/vault_password
+tar -C $SCRIPT_DIR -cvf $SCRIPT_DIR/sensitive.tar sensitive/ $SCRIPT_DIR/ansible/vault_password $SCRIPT_DIR/fix_git.sh
 gpg --yes --symmetric --no-symkey-cache --cipher-algo AES256 $SCRIPT_DIR/sensitive.tar
 rm $SCRIPT_DIR/sensitive.tar
