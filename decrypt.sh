34076ebecedb622fc11b01a5bf4d4a52705fa3bc
Jacob S. Steward <jacobtheeldest@gmail.com>
2023-10-28 19:34:08 +0000

    Only proceed if previous command succeeds

--- decrypt.sh
+++ decrypt.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
-gpg --decrypt $SCRIPT_DIR/sensitive.tar.gpg > $SCRIPT_DIR/sensitive.tar
-tar -C $SCRIPT_DIR -xvf $SCRIPT_DIR/sensitive.tar
-rm $SCRIPT_DIR/sensitive.tar
+gpg --decrypt $SCRIPT_DIR/sensitive.tar.gpg > $SCRIPT_DIR/sensitive.tar && \
+    tar -C $SCRIPT_DIR -xvf $SCRIPT_DIR/sensitive.tar && \
+    rm $SCRIPT_DIR/sensitive.tar
